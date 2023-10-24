9bf2c0c063f4fedacc7d609cfd0f2cf784df1a45
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-10-24 21:15:30 +0200

    added Image on the index.js file

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Querk - First post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
