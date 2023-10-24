bb50c1de8edc2394d41585921344a36be6a0dba8
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-10-24 21:39:30 +0200

    added text on the background photo

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,23 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">this page</Link>
-        </h1>
         <div>
-          <Image
-          src="/images/kitchen.jpg"
-          alt="kitchen"
-          layout="fill"
-          objectFit="cover"
-          />
+          <div className={styles.hero}>
+            <Image
+              src="/images/kitchen.jpg"
+              alt="kitchen"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <h1 className={styles.herotext}>Doskonałość w detalu - Wybierz Querk na drodze do perfekcyjnych mebli.</h1>
         </div>
       </main>
 
       <footer>
+        <h1 className={styles.title}>
+          Read <Link href="/posts/first-post">this page</Link>
+        </h1>
       </footer>
     </div>
   );
